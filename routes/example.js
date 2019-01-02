c752989377f905ecde4b3af9e308945f0c2bf8fa
haolong <haolong@51jianjiao.com>
2019-01-02 10:57:25 +0800

    modify

--- routes/example.js
+++ routes/example.js
@@ -4,13 +4,13 @@ var util = require("util");
 var mysql = require('mysql');
 var db = require('../database/mysql.js');
 
-router.get("/status/:star_name", function(req, res, next) {
+router.get("/status/:name", function(req, res, next) {
     //地址栏中的star_name 与 SQL 结合
     var sql = util.format(
         `select review from star_fans_status where star_id in(
             select id from star_base_info where name=%s
         )`,
-        mysql.escape(req.params.star_name)
+        mysql.escape(req.params.name)
     );
     
     //打印SQL
@@ -33,38 +33,6 @@ router.get("/status/:star_name", function(req, res, next) {
             'success': true,
             // 'data': JSON.parse(rows[0])
             'data': rows
-        });
-    });
-});
-
-router.get("/wrdcld/:star_name", function(req, res, next) {
-    var sql = util.format(
-        `select review from star_review where star_id in(
-            select id from star_base_info where name=%s
-        )`,
-        mysql.escape(req.params.star_name)
-    );
-
-    console.log(sql);
-
-    db.query(sql, function(err, rows, fields) {
-        if (err) {
-            console.log("[query] - :" + err);
-            res.jsonp({'success': false, 'data': []});
-            return;
-        }
-
-        if (!Object.keys(rows).length > 0) {
-            res.jsonp({'success': true, 'data': []});
-            return;
-        }
-
-        // console.log(rows[0]['review']);
-
-        var result = JSON.parse(rows[0]['review']);
-        res.jsonp({
-            'success': true,
-            'data': result
         });
     });
 });
